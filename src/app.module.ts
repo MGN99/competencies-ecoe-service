393a4a1a2e89b684f87f8b08b61f4dc76cdb8876
pwn922 <mauricio41533@gmail.com>
2025-05-17 13:47:48 -0800

    feat(app): integrate global config and database setup in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
+import { CompetenciesEcoeModule } from './competencies-ecoe/competencies-ecoe.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => typeOrmConfig(configService),
+    }),
+    CompetenciesEcoeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
