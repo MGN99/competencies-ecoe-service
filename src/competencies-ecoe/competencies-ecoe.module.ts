305227489000fbb3b07bd0037484d7b902fd34e1
pwn922 <mauricio41533@gmail.com>
2025-05-19 21:40:11 -0800

    feat(competencies-ecoe): register CompetencieController and use cases in CompetenciesEcoeModule

--- src/competencies-ecoe/competencies-ecoe.module.ts
+++ src/competencies-ecoe/competencies-ecoe.module.ts
@@ -1,13 +1,19 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmPersistenceModule } from "./infrastructure/persistence/typeorm/typeorm-persistence.module";
+import { CompetencieController } from "./infrastructure/controllers/competencie.controller";
+import { GetCompetenciesBySubjectUseCase } from "./application/use-cases/get-competencies-by-subject.use-case";
+import { GetStudentEcoeByYearUseCase } from "./application/use-cases/get-student-ecoe-by-year.use-case";
 
 
 @Module({
     imports: [
         TypeOrmPersistenceModule,
     ],
-    controllers: [],
-    providers: [],
+    controllers: [CompetencieController],
+    providers: [
+        GetCompetenciesBySubjectUseCase,
+        GetStudentEcoeByYearUseCase,
+    ],
 })
 
 export class CompetenciesEcoeModule {}
