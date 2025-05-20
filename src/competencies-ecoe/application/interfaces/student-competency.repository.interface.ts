9c6d1bb887ff20408b3c24cdec664220b46445d7
pwn922 <mauricio41533@gmail.com>
2025-05-19 21:36:52 -0800

    feat(competencies-ecoe): remove StudentCompetencyRepository interface

--- src/competencies-ecoe/application/interfaces/student-competency.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { StudentCompetency } from "src/competencies-ecoe/domain/models/student-competency.entity";
-
-export interface StudentCompetencyRepository {
-  findByStudentAndEcoeYear(studentId: string, EcoeYear: number): Promise<StudentCompetency[]>;
-  findBySubject(subjectId: number): Promise<any[]>;
-}
