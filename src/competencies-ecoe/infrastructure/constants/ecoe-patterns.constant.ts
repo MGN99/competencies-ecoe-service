54f6a7a977be81f36403b5cdb2d5a6c2960035fa
Matias364 <matias.cortes09@alumnos.ucn.cl>
2025-05-20 12:18:46 -0400

    feat(avg-ecoe): add patter
    se agregó patron de mensaje para rabbitmq

--- src/competencies-ecoe/infrastructure/constants/ecoe-patterns.constant.ts
+++ src/competencies-ecoe/infrastructure/constants/ecoe-patterns.constant.ts
@@ -1,3 +1,4 @@
 export const EcoeMessagePatterns = {
     GET_STUDENT_ECOE_BY_YEAR: 'get_student_ecoe_by_year',
+    GET_STUDENT_ECOE_COMPETENCIES_AVG_BY_YEAR: 'get_student_ecoe_competencies_avg_by_year',
 };
