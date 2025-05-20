20ed65b6726dd73876a0fcb9b932e52cdcbbcd79
pwn922 <mauricio41533@gmail.com>
2025-05-19 21:39:25 -0800

    feat(competencies-ecoe): create EcoeStudentRepositoryImpl and StudentCompetencyRepositoryImpl

---
+++ src/competencies-ecoe/infrastructure/persistence/typeorm/repositories/ecoe-student.repository.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { IEcoeStudentRepositoryOutPort } from "src/competencies-ecoe/domain/repositories/ecoe-student-repository.out.port";
+import { EcoeStudentEntityOrm } from "../entities/ecoe-student.entity.orm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { EcoeStudent } from "src/competencies-ecoe/domain/models/ecoe-student.entity";
+import { Repository } from "typeorm";
+import { EcoeStudentMapper } from "src/competencies-ecoe/infrastructure/mappers/ecoe-student.mapper";
+
+@Injectable()
+export class EcoeStudentRepositoryImpl implements IEcoeStudentRepositoryOutPort {
+  constructor(
+    @InjectRepository(EcoeStudentEntityOrm)
+    private readonly ormRepo: Repository<EcoeStudentEntityOrm>,
+  ) {}
+
+    async findByStudentIdAndEcoeYear(studentId: string, ecoeYear: number): Promise<EcoeStudent | null> {
+        const ormEntity = await this.ormRepo.findOne({
+            where: {
+            student_id: studentId,
+            ecoe_year: ecoeYear,
+            },
+            relations: ['ecoe', 'competenciesEvaluated', 'competenciesEvaluated.competency'],
+        });
+
+        if (!ormEntity) {
+            return null;
+        }
+
+        return EcoeStudentMapper.toDomain(ormEntity);
+    }
+}
