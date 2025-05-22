a3b1b9186feb314e8d901addb41127325043e20a
pwn922 <mauricio41533@gmail.com>
2025-05-21 20:01:11 -0800

    feat(competencies-ecoe): remove IEcoeStudentRepositoryOutPort

--- src/competencies-ecoe/domain/repositories/ecoe-student-repository.out.port.ts
+++
@@ -1,6 +0,0 @@
-import { EcoeStudent } from "../models/ecoe-student.entity";
-
-
-export interface IEcoeStudentRepositoryOutPort {
-    findByStudentIdAndEcoeYear(studentId: string, ecoeYear: number): Promise<EcoeStudent | null>;
-}
