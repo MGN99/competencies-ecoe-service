c95c56ff78d07a8e54910c2ec7171e748b9fdc21
pwn922 <mauricio41533@gmail.com>
2025-05-21 19:59:25 -0800

    feat(ecoe-student): remove unused repository injection

--- src/competencies-ecoe/application/use-cases/get-student-ecoe-by-year.use-case.ts
+++ src/competencies-ecoe/application/use-cases/get-student-ecoe-by-year.use-case.ts
@@ -1,25 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { IStudentCompetencyRepositoryOutPort } from '../../domain/repositories/student-competency.repository.out.port';
 import { StudentEcoeByYearDto } from '../dtos/student-ecoe-by-year.dto';
-import { IEcoeStudentRepositoryOutPort } from 'src/competencies-ecoe/domain/repositories/ecoe-student-repository.out.port';
+import { IEcoeStudentRepositoryOutPort } from 'src/competencies-ecoe/domain/repositories/ecoe-student.repository.out.port';
 
 @Injectable()
 export class GetStudentEcoeByYearUseCase {
-  constructor(
-    @Inject('IEcoeStudentRepositoryOutPort')
-    private readonly repositoryEcoeStudent: IEcoeStudentRepositoryOutPort,
+    constructor(
+        @Inject('IEcoeStudentRepositoryOutPort')
+        private readonly repositoryEcoeStudent: IEcoeStudentRepositoryOutPort,
+    ) {}
 
-    @Inject('IStudentCompetencyRepositoryOutPort')
-    private readonly repositoryStudentCompetency: IStudentCompetencyRepositoryOutPort,
-  ) {}
+    async execute(dto: StudentEcoeByYearDto): Promise<any> {
+        const ecoeStudent = await this.repositoryEcoeStudent.findByStudentIdAndEcoeYear(dto.studentId, dto.ecoeYear);
 
-  async execute(dto: StudentEcoeByYearDto): Promise<any> {
-    const ecoeStudent = await this.repositoryEcoeStudent.findByStudentIdAndEcoeYear(dto.studentId, dto.ecoeYear);
+        if (!ecoeStudent) {
+            return {};
+        }
 
-    if (!ecoeStudent) {
-      return {};
+        return ecoeStudent;
     }
-
-    return ecoeStudent;
-  }
 }
