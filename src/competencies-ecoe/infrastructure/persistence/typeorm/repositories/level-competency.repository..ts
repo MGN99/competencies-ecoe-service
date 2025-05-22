b7abd8050f51e96d7347d8cfee11a8c7c596ba5a
pwn922 <mauricio41533@gmail.com>
2025-05-21 20:05:41 -0800

    feat(competencies-ecoe): create LevelCompetencyRepository

---
+++ src/competencies-ecoe/infrastructure/persistence/typeorm/repositories/level-competency.repository..ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, In } from 'typeorm';
+import { LevelCompetencyRepositoryOutPort } from 'src/competencies-ecoe/domain/repositories/level-competency.repository.out.port';
+import { LevelCompetencyEntityOrm } from '../entities/level-competency.entity.orm';
+import { LevelCompetency } from 'src/competencies-ecoe/domain/models/level-competency.entity';
+import { LevelsCompetencyMapper } from 'src/competencies-ecoe/infrastructure/mappers/levels-competency.mapper';
+
+
+@Injectable()
+export class LevelCompetencyRepository implements LevelCompetencyRepositoryOutPort {
+    constructor(
+        @InjectRepository(LevelCompetencyEntityOrm)
+        private readonly repo: Repository<LevelCompetencyEntityOrm>,
+    ) {}
+
+    async findManyByIds(ids: number[]): Promise<LevelCompetency[]> {
+        const entities = await this.repo.find({
+            where: { id: In(ids) },
+            relations: ['competency'],
+        });
+
+        return LevelsCompetencyMapper.toDomainList(entities);
+    }
+}
