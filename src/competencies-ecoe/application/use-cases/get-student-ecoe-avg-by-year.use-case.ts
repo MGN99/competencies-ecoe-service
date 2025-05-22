059859a7ea15270ddde2abd9182f882891161f89
pwn922 <mauricio41533@gmail.com>
2025-05-21 19:59:14 -0800

    fix(avg-ecoe): fix return average and check competenciesEvaluated

--- src/competencies-ecoe/application/use-cases/get-student-ecoe-avg-by-year.use-case.ts
+++ src/competencies-ecoe/application/use-cases/get-student-ecoe-avg-by-year.use-case.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { IEcoeStudentRepositoryOutPort } from 'src/competencies-ecoe/domain/repositories/ecoe-student-repository.out.port';
+import { IEcoeStudentRepositoryOutPort } from 'src/competencies-ecoe/domain/repositories/ecoe-student.repository.out.port';
 import { StudentEcoeCompetenciesAvgByYearDto } from '../dtos/student-ecoe-avg-by-year.dto';
 
 @Injectable()
@@ -10,10 +10,11 @@ export class GetStudentEcoeCompetenciesAvgByYearUseCase {
     ) {}
     async execute(dto: StudentEcoeCompetenciesAvgByYearDto): Promise<{ average: number }> {
         const ecoeStudent = await this.repositoryEcoeStudent.findByStudentIdAndEcoeYear(dto.studentId, dto.ecoeYear);
-        if (!ecoeStudent || ecoeStudent.competenciesEvaluated || ecoeStudent.competenciesEvaluated.length === 0) {
+        if (!ecoeStudent || !ecoeStudent.competenciesEvaluated || ecoeStudent.competenciesEvaluated.length === 0) {
             return { average: 0 };
         }
         const sum = ecoeStudent.competenciesEvaluated.reduce((acc, competency) => acc + (competency.grade ?? 0), 0);
         const average = sum / ecoeStudent.competenciesEvaluated.length;
+        return { average };
     }
 }
