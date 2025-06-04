01fae8fdec7f83db0ae0b5ec525d27216e0e9686
pwn922 <mauricio41533@gmail.com>
2025-06-03 22:40:59 -0800

    style(ecoe-student): format code for improved readability and consistency

--- src/competencies-ecoe/infrastructure/persistence/typeorm/entities/ecoe-student.entity.orm.ts
+++ src/competencies-ecoe/infrastructure/persistence/typeorm/entities/ecoe-student.entity.orm.ts
@@ -1,32 +1,40 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+} from "typeorm";
 import { EcoeEntityOrm } from "./ecoe.entity.orm";
 import { StudentCompetencyEntityOrm } from "./student-competency.entity.orm";
 
-
 @Entity('ecoe_student')
+//@Unique(['student_id', 'ecoe_year'])
 export class EcoeStudentEntityOrm {
-    @PrimaryGeneratedColumn('increment')
-    id: number;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
-    @Column()
-    student_id: string;
+  @Column()
+  student_id: string;
 
-    @Column('numeric')
-    final_note: number;
+  @Column('numeric')
+  final_note: number;
 
-    @Column()
-    final_achievement_level: string;
+  @Column()
+  final_achievement_level: string;
 
-    @Column('numeric')
-    ecoe_year: number;
+  @Column('numeric')
+  ecoe_year: number;
 
-    @ManyToOne(() => EcoeEntityOrm, ecoe => ecoe.students)
-    @JoinColumn({ name: 'ecoe_id' })
-    ecoe: EcoeEntityOrm;
+  @ManyToOne(() => EcoeEntityOrm, ecoe => ecoe.students)
+  @JoinColumn({ name: 'ecoe_id' })
+  ecoe: EcoeEntityOrm;
 
-    @OneToMany(() => StudentCompetencyEntityOrm, sc => sc.ecoeStudent, {
-        cascade: true,
-        eager: true,
-    })
-    competenciesEvaluated: StudentCompetencyEntityOrm[];
+  @OneToMany(() => StudentCompetencyEntityOrm, sc => sc.ecoeStudent, {
+    cascade: true,
+    eager: true,
+  })
+  competenciesEvaluated: StudentCompetencyEntityOrm[];
 }
