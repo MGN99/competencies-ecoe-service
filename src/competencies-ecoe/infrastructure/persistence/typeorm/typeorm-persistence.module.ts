0c2e88f6bcb2819ce98b5604bd8ec173788cc6bf
pwn922 <mauricio41533@gmail.com>
2025-06-03 22:42:56 -0800

    fix(typeorm-persistence-module): correct import path for LevelCompetencyRepository and add ICompetencyRepositoryOutPort provider

--- src/competencies-ecoe/infrastructure/persistence/typeorm/typeorm-persistence.module.ts
+++ src/competencies-ecoe/infrastructure/persistence/typeorm/typeorm-persistence.module.ts
@@ -7,7 +7,8 @@ import { LevelCompetencyEntityOrm } from "./entities/level-competency.entity.orm
 import { StudentCompetencyEntityOrm } from "./entities/student-competency.entity.orm";
 import { StudentCompetencyRepositoryImpl } from "./repositories/student-competency.repository";
 import { EcoeStudentRepositoryImpl } from "./repositories/ecoe-student.repository";
-import { LevelCompetencyRepository } from "./repositories/level-competency.repository.";
+import { LevelCompetencyRepository } from "./repositories/level-competency.repository";
+import { CompetencyRepository } from "./repositories/competency.repository";
 
 
 @Module({
@@ -31,12 +32,17 @@ import { LevelCompetencyRepository } from "./repositories/level-competency.repos
         {
             provide: 'LevelCompetencyRepositoryOutPort',
             useClass: LevelCompetencyRepository,
+        },
+        {
+            provide: 'ICompetencyRepositoryOutPort',
+            useClass: CompetencyRepository,
         }
     ],
     exports: [
         'IStudentCompetencyRepositoryOutPort',
         'IEcoeStudentRepositoryOutPort',
         'LevelCompetencyRepositoryOutPort',
+        'ICompetencyRepositoryOutPort',
     ]
 })
 export class TypeOrmPersistenceModule {}
