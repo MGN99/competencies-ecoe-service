ac363e5302409a3eed28d70bb93ac81ad1f04580
pwn922 <mauricio41533@gmail.com>
2025-06-04 15:59:53 -0800

    feat(ecoe-repository): enhance EcoeRepositoryOutPort with findById method

--- src/competencies-ecoe/domain/repositories/ecoe.repository.out.port.ts
+++ src/competencies-ecoe/domain/repositories/ecoe.repository.out.port.ts
@@ -1,5 +1,7 @@
 import { Ecoe } from '../models/ecoe.entity';
 
-export interface EcoeRepositoryOutPort {
+export interface IEcoeRepositoryOutPort {
   findAvailable(): Promise<Pick<Ecoe, 'id' | 'semester' | 'description'>[]>;
+
+  findById(id: number): Promise<Ecoe | null>;
 }
