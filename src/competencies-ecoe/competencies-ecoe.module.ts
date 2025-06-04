d560c7928e8e6bc8c25dbfc1f47428832acb24da
Miguel Galleguillos <miguel.galleguillos01@alumnos.ucn.cl>
2025-06-04 18:23:01 -0400

    feat(competencies-ecoe-module): add EcoesController and GetAvailableEcoesUseCase to module

--- src/competencies-ecoe/competencies-ecoe.module.ts
+++ src/competencies-ecoe/competencies-ecoe.module.ts
@@ -7,13 +7,17 @@ import { GetCompetenciesLevelByIdsUseCase } from "./application/use-cases/get-co
 import { GetStudentEcoeYearsUseCase } from "./application/use-cases/get-student-ecoe-years.use-case";
 import { GetLevelCompetencyIdsByCompetencyIdUseCase } from "./application/use-cases/get-level-competency-ids-by-competency-id.use-case";
 import { GetCompetencyByIdUseCase } from "./application/use-cases/get-competency-by-id.use-case";
+import { Ecoe } from "./domain/models/ecoe.entity";
+import { EcoesController } from "./infrastructure/controllers/ecoes.controller";
+import { get } from "http";
+import { GetAvailableEcoesUseCase } from "./application/use-cases/get-ecoes-availables.use-case";
 
 
 @Module({
     imports: [
         TypeOrmPersistenceModule,
     ],
-    controllers: [CompetencieController],
+    controllers: [CompetencieController,EcoesController],
     providers: [
         GetCompetenciesLevelByIdsUseCase,
         GetStudentEcoeByYearUseCase,
@@ -21,6 +25,8 @@ import { GetCompetencyByIdUseCase } from "./application/use-cases/get-competency
         GetStudentEcoeYearsUseCase,
         GetLevelCompetencyIdsByCompetencyIdUseCase,
         GetCompetencyByIdUseCase,
+        GetAvailableEcoesUseCase,
+        
     ],
 })
 
