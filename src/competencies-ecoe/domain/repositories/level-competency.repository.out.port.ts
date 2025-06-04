8512e7d771a0c91453559ab3d13ce0f28e5d47e8
pwn922 <mauricio41533@gmail.com>
2025-06-03 22:40:36 -0800

    feat(competencies-ecoe): add findByCompetencyId method to LevelCompetencyRepositoryOutPort interface

--- src/competencies-ecoe/domain/repositories/level-competency.repository.out.port.ts
+++ src/competencies-ecoe/domain/repositories/level-competency.repository.out.port.ts
@@ -3,4 +3,6 @@ import { LevelCompetency } from "../models/level-competency.entity";
 
 export interface LevelCompetencyRepositoryOutPort {
     findManyByIds(ids: number[]): Promise<LevelCompetency[]>
+
+    findByCompetencyId(competencyId: number): Promise<LevelCompetency[]>
 }
