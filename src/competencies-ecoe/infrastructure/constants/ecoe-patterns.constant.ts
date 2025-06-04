c270ec8cdaf4e31588d80c1fe6fd9a12679133c2
pwn922 <mauricio41533@gmail.com>
2025-06-03 22:40:47 -0800

    feat(ecoe-patterns): add new message patterns for ecoe functionalities

--- src/competencies-ecoe/infrastructure/constants/ecoe-patterns.constant.ts
+++ src/competencies-ecoe/infrastructure/constants/ecoe-patterns.constant.ts
@@ -2,4 +2,7 @@ export const EcoeMessagePatterns = {
     GET_STUDENT_ECOE_BY_YEAR: 'get_student_ecoe_by_year',
     GET_STUDENT_ECOE_COMPETENCIES_AVG_BY_YEAR: 'get_student_ecoe_competencies_avg_by_year',
     GET_COMPETENCIES_LEVEL_BY_IDS: 'get_competencies_level_by_ids',
+    GET_STUDENT_ECOE_YEARS: 'get_student_ecoe_years',
+    GET_LEVEL_COMPETENCIES_BY_COMPETENCY_ID: 'get_level_competencies_by_competency_id',
+    EXISTS_COMPETENCY_BY_ID: 'exists_competency_by_id',
 };
