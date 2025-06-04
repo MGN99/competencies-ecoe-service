2c169d306ecda63b079eee51df5939d68454e345
pwn922 <mauricio41533@gmail.com>
2025-06-04 15:59:44 -0800

    feat(ecoe-student-repository): add methods for student-ecoe association

--- src/competencies-ecoe/domain/repositories/ecoe-student.repository.out.port.ts
+++ src/competencies-ecoe/domain/repositories/ecoe-student.repository.out.port.ts
@@ -1,8 +1,13 @@
 import { EcoeStudent } from "../models/ecoe-student.entity";
+import { Ecoe } from "../models/ecoe.entity";
 
 
 export interface IEcoeStudentRepositoryOutPort {
     findByStudentIdAndEcoeYear(studentId: string, ecoeYear: number): Promise<EcoeStudent | null>;
 
     findEcoeYearsByStudentId(studentId: string): Promise<number[]>;
+
+    addStudent(studentId: string, ecoeId: number, year: number): Promise<void>;
+
+    existsStudentInEcoeYear(studentId: string, ecoeYear: number): Promise<boolean>;
 }
