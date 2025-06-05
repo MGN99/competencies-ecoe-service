3f3da76e46eed91e3c4d10e6769e562af02ab610
pwn922 <mauricio41533@gmail.com>
2025-06-04 22:42:03 -0800

    feat(competencies-ecoe): implement getStudentsByEcoeId endpoint

--- src/competencies-ecoe/infrastructure/controllers/ecoes.controller.ts
+++ src/competencies-ecoe/infrastructure/controllers/ecoes.controller.ts
@@ -1,8 +1,12 @@
-import { Body, ConflictException, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { GetAvailableEcoesUseCase } from 'src/competencies-ecoe/application/use-cases/get-ecoes-availables.use-case';
 import { AddStudentToEcoeDto } from '../dtos/add-student-to-ecoe.dto';
 import { AddStudentToEcoeCurrentYearUseCase } from 'src/competencies-ecoe/application/use-cases/add-student-to-ecoe-current-year.use-case';
 import { StudentAlreadyInEcoeYearError } from 'src/competencies-ecoe/domain/errors/student-already-in-ecoe-year.error';
+import { GetEcoeStudentsUseCase } from 'src/competencies-ecoe/application/use-cases/get-ecoe-students.use-case';
+import { GetStudentsByEcoeIdDto } from '../dtos/get-students-by-ecoe-id.dto';
+import { EcoeNotFoundError } from 'src/competencies-ecoe/domain/errors/ecoe-not-found.error';
+import { EcoeStudentMapper } from '../mappers/ecoe-student.mapper';
 
 
 @Controller('/api/v1/ecoes')
@@ -10,6 +14,7 @@ export class EcoesController {
   constructor(
     private readonly getAvailableEcoesUseCase: GetAvailableEcoesUseCase,
     private readonly addStudentToEcoeCurrentYearUseCase: AddStudentToEcoeCurrentYearUseCase,
+    private readonly getEcoeStudentsUseCase: GetEcoeStudentsUseCase,
 ) {}
 
   @Get('available')
@@ -30,5 +35,22 @@ export class EcoesController {
 
       throw error;
     }
-  }
+    }
+
+    @Get(':ecoeId/students')
+    async getStudentsByEcoeId(@Param() data: GetStudentsByEcoeIdDto) {
+        try {
+            const ecoeStudents = await this.getEcoeStudentsUseCase.execute(data.ecoeId);
+            const ecoeStudentResponse = ecoeStudents.map(ecoeStudent => {
+                return EcoeStudentMapper.toResponseDto(ecoeStudent);
+            });
+            
+            return ecoeStudentResponse;
+        } catch (error) {
+            if (error instanceof EcoeNotFoundError) {
+                throw new ConflictException(error.message);
+            }
+            throw error;
+        }
+    }
 }
