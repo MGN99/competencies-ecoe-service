43e1fe53966370098a279d309e011124ef41e7de
pwn922 <mauricio41533@gmail.com>
2025-06-04 16:02:43 -0800

    feat(main): add global validation pipe to bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
         },
     });
     
+    app.useGlobalPipes(new ValidationPipe());
     await app.startAllMicroservices();
 
     await app.listen(3002);
