09288e85fa075be667b4f060d1cb19e14de7d0b3
pwn922 <mauricio41533@gmail.com>
2025-06-04 22:42:54 -0800

    refactor(ecoe-student.mapper): improve formatting and structure of EcoeStudentMapper methods

--- src/competencies-ecoe/infrastructure/mappers/ecoe-student.mapper.ts
+++ src/competencies-ecoe/infrastructure/mappers/ecoe-student.mapper.ts
@@ -5,21 +5,33 @@ import { EcoeStudentEntityOrm } from "../persistence/typeorm/entities/ecoe-stude
 import { Ecoe } from "src/competencies-ecoe/domain/models/ecoe.entity";
 import { StudentCompetencyMapper } from "./student-competency.mapper";
 import { EcoeMapper } from "./ecoe.mapper";
+import { EcoeStudentResponseDto } from "../dtos/ecoe-student-response.dto";
 
 
 export class EcoeStudentMapper {
-  static toDomain(entity: EcoeStudentEntityOrm): EcoeStudent {
-    return new EcoeStudent(
-      entity.id,
-      entity.student_id,
-      new Ecoe(
-        entity.ecoe.id,
-        entity.ecoe.semester,
-        entity.ecoe.description
-      ),
-      entity.final_note,
-      entity.final_achievement_level,
-      entity.competenciesEvaluated?.map(StudentCompetencyMapper.toDomain) ?? []
-    );
-  }
+    static toDomain(entity: EcoeStudentEntityOrm): EcoeStudent {
+        return new EcoeStudent(
+            entity.id,
+            entity.student_id,
+            new Ecoe(
+                entity.ecoe.id,
+                entity.ecoe.semester,
+                entity.ecoe.description
+            ),
+            entity.final_note,
+            entity.final_achievement_level,
+            entity.competenciesEvaluated?.map(StudentCompetencyMapper.toDomain) ?? []
+        );
+    }
+
+    static toResponseDto(domain: EcoeStudent): EcoeStudentResponseDto {
+        return {
+            id: domain.id,
+            studentId: domain.studentId,
+            ecoeId: domain.ecoe.id,
+            competencyEvaluatedIds: domain.competenciesEvaluated.map(
+                (c) => c.id
+            ),
+        };
+    }
 }
