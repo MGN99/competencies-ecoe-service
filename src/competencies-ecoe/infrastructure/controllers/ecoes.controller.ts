0f2b36f2eb2701f80e3db948eb7103aaa1c6f8d8
pwn922 <mauricio41533@gmail.com>
2025-06-04 16:00:12 -0800

    feat(ecoes-controller): add addStudentToEcoe method for student registration

--- src/competencies-ecoe/infrastructure/controllers/ecoes.controller.ts
+++ src/competencies-ecoe/infrastructure/controllers/ecoes.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, ConflictException, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { GetAvailableEcoesUseCase } from 'src/competencies-ecoe/application/use-cases/get-ecoes-availables.use-case';
+import { AddStudentToEcoeDto } from '../dtos/add-student-to-ecoe.dto';
+import { AddStudentToEcoeCurrentYearUseCase } from 'src/competencies-ecoe/application/use-cases/add-student-to-ecoe-current-year.use-case';
+import { StudentAlreadyInEcoeYearError } from 'src/competencies-ecoe/domain/errors/student-already-in-ecoe-year.error';
 
 
-@Controller('ecoes')
+@Controller('/api/v1/ecoes')
 export class EcoesController {
-  constructor(private readonly getAvailableEcoesUseCase: GetAvailableEcoesUseCase) {}
+  constructor(
+    private readonly getAvailableEcoesUseCase: GetAvailableEcoesUseCase,
+    private readonly addStudentToEcoeCurrentYearUseCase: AddStudentToEcoeCurrentYearUseCase,
+) {}
 
   @Get('available')
   async getAvailableEcoes() {
     return this.getAvailableEcoesUseCase.execute();
+  }
+
+  @Post('add-student-to-ecoe')
+  @HttpCode(201)
+  async addStudentToEcoe(@Body() data: AddStudentToEcoeDto) {
+    try {
+      await this.addStudentToEcoeCurrentYearUseCase.execute(data);
+      return { message: 'Student added successfully to current ECOE' };
+    } catch (error) {
+      if (error instanceof StudentAlreadyInEcoeYearError) {
+        throw new ConflictException(error.message);
+      }
+
+      throw error;
+    }
   }
 }
