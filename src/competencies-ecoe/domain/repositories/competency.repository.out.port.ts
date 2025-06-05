1ff28e5ed868f16e2867c042d08073c03f3dd4b4
pwn922 <mauricio41533@gmail.com>
2025-06-04 22:41:18 -0800

    feat(competencies-ecoe): add EcoeStudent import to competency repository port

--- src/competencies-ecoe/domain/repositories/competency.repository.out.port.ts
+++ src/competencies-ecoe/domain/repositories/competency.repository.out.port.ts
@@ -1,4 +1,5 @@
 import { Competency } from "../models/competency.entity";
+import { EcoeStudent } from "../models/ecoe-student.entity";
 
 export interface ICompetencyRepositoryOutPort {
     findById(id: number): Promise<Competency | null>;
