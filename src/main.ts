b0b8fa5fabdddd83b14f8bd378193d198a6a8a06
Matias364 <matias.cortes09@alumnos.ucn.cl>
2025-06-06 15:11:47 -0400

    refactor(CORS in main): Cors has been enabled for connection to the frontend.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
 
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    const frontendUrl = configService.get<string>('FRONTEND_URL') || 'http://localhost:5173';
+
+    app.enableCors({
+        origin: frontendUrl,
+    });
 
     app.connectMicroservice<MicroserviceOptions>({
             transport: Transport.RMQ,
