a565388e176583e1596a6ec8100b138ab440f2dc
pwn922 <mauricio.alarcon@alumnos.ucn.cl>
2025-06-10 16:30:18 -0400

    feat(ecoe.repository): adding createEcoeInstance, findInstancesByEcoeID and findAll

--- src/competencies-ecoe/domain/repositories/ecoe.repository.out.port.ts
+++ src/competencies-ecoe/domain/repositories/ecoe.repository.out.port.ts
@@ -1,7 +1,18 @@
 import { Ecoe } from '../models/ecoe.entity';
+import { EcoeInstance } from '../models/ecoe_instance.entity';
 
 export interface IEcoeRepositoryOutPort {
-  findAvailable(): Promise<Pick<Ecoe, 'id' | 'semester' | 'description'>[]>;
+  //findAvailable(): Promise<Pick<Ecoe, 'id' | 'semester' | 'description'>[]>;
+  findAll(): Promise<Ecoe[]>;
 
   findById(id: number): Promise<Ecoe | null>;
+
+  findInstancesByEcoeId(ecoeId: number): Promise<EcoeInstance[]>
+
+  createEcoeInstance(
+    ecoeId: number,
+    year: number,
+    semester: number,
+    description: string,
+  ): Promise<EcoeInstance>
 }
