c6858633af74e9a8bef5edfadcc5b04187fe9197
pwn922 <mauricio.alarcon@alumnos.ucn.cl>
2025-06-10 16:34:02 -0400

    refactor(student-competency.mapper): adding mappers

--- src/competencies-ecoe/infrastructure/mappers/student-competency.mapper.ts
+++ src/competencies-ecoe/infrastructure/mappers/student-competency.mapper.ts
@@ -1,18 +1,18 @@
-import { StudentCompetency } from "src/competencies-ecoe/domain/models/student-competency.entity";
+import { StudentLevelCompetency } from "src/competencies-ecoe/domain/models/student-level-competency.entity";
 import { StudentCompetencyEntityOrm } from "../persistence/typeorm/entities/student-competency.entity.orm";
 import { Competency } from "src/competencies-ecoe/domain/models/competency.entity";
+import { EcoeStudentMapper } from "./ecoe-student.mapper";
+import { LevelsCompetencyMapper } from "./levels-competency.mapper";
 
 export class StudentCompetencyMapper {
-  static toDomain(entity: StudentCompetencyEntityOrm): StudentCompetency {
-    return new StudentCompetency(
+  static toDomain(entity: StudentCompetencyEntityOrm): StudentLevelCompetency {
+
+    return new StudentLevelCompetency(
       entity.id,
-      new Competency(
-        entity.competency.id,
-        entity.competency.name,
-        entity.competency.description
-      ),
       entity.grade,
-      entity.level_achievement
+      entity.levelAchievement,
+      EcoeStudentMapper.toDomain(entity.ecoeStudent),
+      LevelsCompetencyMapper.toDomain(entity.levelCompetency),
     );
   }
 }
