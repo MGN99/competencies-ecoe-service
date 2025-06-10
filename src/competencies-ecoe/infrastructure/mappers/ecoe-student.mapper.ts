77638f9da0bb0a980e7738ac3b544c651da8c233
pwn922 <mauricio.alarcon@alumnos.ucn.cl>
2025-06-10 16:32:53 -0400

    refactor(ecoe-student.mapper): adding EcoeInstanceMapper and rename field entity

--- src/competencies-ecoe/infrastructure/mappers/ecoe-student.mapper.ts
+++ src/competencies-ecoe/infrastructure/mappers/ecoe-student.mapper.ts
@@ -1,25 +1,18 @@
-// EcoeStudentMapper.ts
-
 import { EcoeStudent } from "src/competencies-ecoe/domain/models/ecoe-student.entity";
 import { EcoeStudentEntityOrm } from "../persistence/typeorm/entities/ecoe-student.entity.orm";
-import { Ecoe } from "src/competencies-ecoe/domain/models/ecoe.entity";
 import { StudentCompetencyMapper } from "./student-competency.mapper";
-import { EcoeMapper } from "./ecoe.mapper";
 import { EcoeStudentResponseDto } from "../dtos/ecoe-student-response.dto";
+import { EcoeInstanceMapper } from "./ecoe-instance.mapper";
 
 
 export class EcoeStudentMapper {
     static toDomain(entity: EcoeStudentEntityOrm): EcoeStudent {
         return new EcoeStudent(
             entity.id,
-            entity.student_id,
-            new Ecoe(
-                entity.ecoe.id,
-                entity.ecoe.semester,
-                entity.ecoe.description
-            ),
-            entity.final_note,
-            entity.final_achievement_level,
+            entity.studentId,
+            EcoeInstanceMapper.toDomain(entity.ecoeInstance),
+            entity.finalNote,
+            entity.finalArchievementLevel,
             entity.competenciesEvaluated?.map(StudentCompetencyMapper.toDomain) ?? []
         );
     }
@@ -28,8 +21,8 @@ export class EcoeStudentMapper {
         return {
             id: domain.id,
             studentId: domain.studentId,
-            ecoeId: domain.ecoe.id,
-            competencyEvaluatedIds: domain.competenciesEvaluated.map(
+            ecoeInstanceId: domain.ecoeInstance.id,
+            competencyEvaluatedIds: domain.levelCompetenciesEvaluated.map(
                 (c) => c.id
             ),
         };
