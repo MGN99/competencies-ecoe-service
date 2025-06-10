df3f26b8b8381a945c45bcef58a1bc1ee6405d52
pwn922 <mauricio.alarcon@alumnos.ucn.cl>
2025-06-10 16:23:21 -0400

    refactor(application): replace ecoeId with ecoeInstanceId in AddStudentToEcoeCommand

--- src/competencies-ecoe/application/commands/add-student-to-ecoe.command.ts
+++ src/competencies-ecoe/application/commands/add-student-to-ecoe.command.ts
@@ -1,6 +1,6 @@
 export class AddStudentToEcoeCommand {
     constructor(
         public readonly studentId: string,
-        public readonly ecoeId: number,
+        public readonly ecoeInstanceId: number,
     ) {}
 }
