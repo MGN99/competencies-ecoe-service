12647beafd12649647c99557533247caa2ccffdd
pwn922 <mauricio41533@gmail.com>
2025-07-02 17:39:48 -0400

    feat: implement use case to retrieve students with pending ecoe by cycle

---
+++ src/competencies-ecoe/application/use-cases/get-students-with-pending-ecoe-by-cycle.use-case.ts
@@ -0,0 +1,48 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
+import { firstValueFrom } from "rxjs";
+import { EcoeStudent } from "src/competencies-ecoe/domain/models/ecoe-student.entity";
+import { IEcoeStudentRepositoryOutPort } from "src/competencies-ecoe/domain/repositories/ecoe-student.repository.out.port";
+import { IEcoeRepositoryOutPort } from "src/competencies-ecoe/domain/repositories/ecoe.repository.out.port";
+import { StudentDetailsDto } from "../dtos/student-details.dto";
+
+@Injectable()
+export class GetStudentsWithPendingEcoeByCycleUseCase {
+    constructor(
+        @Inject('STUDENT_SERVICE')
+        private readonly studentClient: ClientProxy,
+        @Inject('IEcoeRepositoryOutPort')
+        private readonly ecoeRepository: IEcoeRepositoryOutPort,
+        @Inject('IEcoeStudentRepositoryOutPort')
+        private readonly ecoeStudentRepository: IEcoeStudentRepositoryOutPort,
+    ) { }
+
+    async execute(cycle: "BASICO" | "PROFESIONAL" | "FINAL"): Promise<StudentDetailsDto[]> {
+        const ecoes = await this.ecoeRepository.findAllByCycle(cycle);
+        if (ecoes.length === 0) return [];
+
+        const ecoeIds = ecoes.map(e => e.id);
+        const ecoeStudents: EcoeStudent[] = await this.ecoeStudentRepository.findByEcoeIds(ecoeIds);
+
+        console.log(ecoeStudents);
+        const students: StudentDetailsDto[] = await firstValueFrom(
+            this.studentClient.send('GET_ALL_STUDENTS', {})
+        );
+
+        console.log(students);
+
+        const filtered = students.filter(student => {
+            const records = ecoeStudents.filter(e => e.studentId === student.id);
+
+            if (records.length === 0) return true;
+
+            return records.every(e =>
+                e.finalGrade !== null &&
+                e.finalGrade > 0 &&
+                e.finalGrade < 4
+            );
+        });
+
+        return filtered;
+    }
+}
