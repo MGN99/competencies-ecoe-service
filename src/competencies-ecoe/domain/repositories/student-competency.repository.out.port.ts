fc7cd85d01ea342ad19771b0a30805c1120c6595
pwn922 <mauricio41533@gmail.com>
2025-07-02 22:03:31 -0400

    feat: add findOneByEcoeStudentAndCompetency method to IStudentCompetencyRepositoryOutPort

--- src/competencies-ecoe/domain/repositories/student-competency.repository.out.port.ts
+++ src/competencies-ecoe/domain/repositories/student-competency.repository.out.port.ts
@@ -2,5 +2,9 @@ import { StudentCompetency } from "src/competencies-ecoe/domain/models/student-c
 
 export interface IStudentCompetencyRepositoryOutPort {
     findByEcoeStudentId(ecoeStudentId: number): Promise<StudentCompetency[]>;
+
+    findOneByEcoeStudentAndCompetency(ecoeStudentId: number, competencyId: number): Promise<StudentCompetency | undefined>;
+
+    save(studentCompetency: StudentCompetency): Promise<void>;
     //findBySubject(subjectId: number): Promise<any[]>;
 }
