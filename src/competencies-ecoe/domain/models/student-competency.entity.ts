ad3d06475635880db06a43d780746ececd1ca004
pwn922 <mauricio41533@gmail.com>
2025-07-02 22:03:27 -0400

    fix: update StudentCompetency constructor to make id optional

--- src/competencies-ecoe/domain/models/student-competency.entity.ts
+++ src/competencies-ecoe/domain/models/student-competency.entity.ts
@@ -3,9 +3,9 @@ import { EcoeStudent } from './ecoe-student.entity';
 
 export class StudentCompetency {
   constructor(
-    public readonly id: number,
     public grade: number,
     public readonly ecoeStudent: EcoeStudent,
     public competency: Competency,
+    public readonly id?: number,
   ) {}
 }
